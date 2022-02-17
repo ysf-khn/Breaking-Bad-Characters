88b3a1743e084e61ed262a3e2a6f3eb71dd8268d
Yusuf <yusufmohd72@gmail.com>
2022-02-18 00:36:30 +0530

    Added pagination

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Header from './components/Header';
 import Search from './components/Search';
 
 import { useEffect, useState } from 'react';
+import Pagination from './components/Pagination';
 
 function App() {
   const [characters, setCharacters] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [query, setQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [charactersPerPage] = useState(8);
 
   useEffect(() => {
     const fetchCharacters = async () => {
@@ -16,21 +19,36 @@ function App() {
       );
       const data = await response.json();
       setCharacters(data);
+
       console.log(data);
       setIsLoading(false);
     };
     fetchCharacters();
   }, [query]);
 
+  const indexOfLastCharacter = currentPage * charactersPerPage;
+  const indexOfFirstCharacter = indexOfLastCharacter - charactersPerPage;
+  const currentCharacters = characters.slice(
+    indexOfFirstCharacter,
+    indexOfLastCharacter
+  );
+
   const queryHandler = (queryValue) => {
     setQuery(queryValue);
   };
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div className="App">
       <Header />
       <Search onQuery={queryHandler} />
-      <CharactersList characters={characters} isLoading={isLoading} />
+      <CharactersList characters={currentCharacters} isLoading={isLoading} />
+      <Pagination
+        charactersPerPage={charactersPerPage}
+        totalCharacters={characters.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
